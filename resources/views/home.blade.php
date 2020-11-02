9b8fdebc118c4f455f9e7295f31599a153bf2e42
geothomas <geothomas@cet.ac.in>
2020-11-02 06:55:19 +0530

    form changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,34 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+   <div class="row">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+   <div class="col-3 p-5">
+   <img src="https://instagram.fadb3-1.fna.fbcdn.net/v/t51.2885-19/44884218_345707102882519_2446069589734326272_n.jpg?_nc_ht=instagram.fadb3-1.fna.fbcdn.net&_nc_ohc=szIAI9ztqlYAX9WlUGH&oh=0e3734af46a0ebf683dd838e3722812b&oe=5FC99B8F&ig_cache_key=YW5vbnltb3VzX3Byb2ZpbGVfcGlj.2" class="rounded-circle" style="height:100px;width:100px">
+   </div>
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+   <div class="col-9 p-5">
+   <div> <h1>geo_thomas1</h1></div>
+   <div class="d-flex">
+   <div class="pr-5"><strong>3</strong>posts</div>
+   <div class="pr-5"><strong>112</strong>followers</div>
+   <div class="pr-5"><strong>212</strong>following</div>
+   </div>
+   <div class="pt-4 font-weight-bold">Geo Thomas</div>
+   <div class="pt-2">CETian</div>
+   <div class="pt-1 font-weight-bold "><a href="https://geothomas1.github.io/">geothomas.github.io</div>
+   </div>
+   <div class="row pt-5">
+   <div class="col-3">
+   <img src=" https://instagram.fcok10-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/90426820_753057671890226_6880011046644812031_n.jpg?_nc_ht=instagram.fcok10-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=cVgC8POltRYAX9OxsMN&_nc_tp=24&oh=0af5eefba5e76553c511e45f1e237a66&oe=5FCA9EA3" class='w-100'>
+   </div>
+   <div class="col-3">
+   <img src="https://instagram.fcok10-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/83994809_1064295913950978_2106221351049660589_n.jpg?_nc_ht=instagram.fcok10-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=rv25ciZ7-0oAX_MIpNE&_nc_tp=24&oh=75548fe92156d8cd5762f81d546e1248&oe=5FC76988 " class="w-100">
+   </div>
+   <div class="col-3">
+   <img src="https://instagram.fcok10-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/84521198_120573929388131_3243810195908607821_n.jpg?_nc_ht=instagram.fcok10-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=V5YwLgVuV7EAX8VX8Mv&_nc_tp=24&oh=9062167cadcd958eb96f449bb6ca86da&oe=5FC7446E" class="w-100">
+   </div>
+</div>
+   </div>
 </div>
 @endsection
