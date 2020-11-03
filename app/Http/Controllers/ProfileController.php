3483ebc8f0a18e3e121ce03d82dac8352d61064a
geothomas <geothomas@cet.ac.in>
2020-11-03 07:50:38 +0530

    set userlogin page with passing value to profile controller via froom web,php with dd()

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,9 +7,9 @@ use Illuminate\Http\Request;
 class ProfileController extends Controller
 {
     //
-    public function index()
+    public function index($user)
     {
+        dd($user);
         return view('home');
     }
-}
 }
